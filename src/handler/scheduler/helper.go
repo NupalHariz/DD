d0643409f81ce917a857bdd0cd0fee51cc9be661
Naufal Haris <naufal.michiels@gmail.com>
2025-06-26 20:14:40 +0700

    feat(scheduler): set up cron job for scheduler

---
+++ src/handler/scheduler/helper.go
@@ -0,0 +1,70 @@
+package scheduler
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/reyhanmichiels/go-pkg/v2/appcontext"
+)
+
+const (
+	schedulerUserAgent = "cron scheduler: %s"
+
+	schedulerRunning      string = "scheduler %s is running"
+	schedulerDoneSuccess  string = "scheduler %s is success"
+	schedulerDoneError    string = "scheduler %s is error: %v"
+	scheduleTimeExecution string = "scheduler %s done in %v"
+
+	schedulerTypeWeekly  string = "weekly"
+	schedulerTypeMonthly string = "monthly"
+)
+
+type handlerFunc func(ctx context.Context) error
+
+func (s *scheduler) createContext(conf SchedulerTaskConf) context.Context {
+	ctx := context.Background()
+
+	ctx = appcontext.SetUserAgent(ctx, fmt.Sprintf(schedulerUserAgent, conf.Name))
+	ctx = appcontext.SetRequestId(ctx, uuid.NewString())
+	ctx = appcontext.SetRequestStartTime(ctx, time.Now())
+	ctx = appcontext.SetServiceVersion(ctx, s.metaConf.Version)
+
+	return ctx
+}
+
+func (s *scheduler) assignTask(conf SchedulerTaskConf, task handlerFunc) {
+	if conf.Enabled {
+		var err error
+		ctx := context.Background()
+		shcedulerFunc := s.taskWrapper(conf, task)
+
+		switch conf.TimeType {
+		case schedulerTypeWeekly:
+			_, err = s.cron.Week().Sunday().Tag(conf.Name).At(conf.ScheduledTime).Do(shcedulerFunc)
+		case schedulerTypeMonthly:
+			_, err = s.cron.Month(-1).Tag(conf.Name).At(conf.ScheduledTime).Do(shcedulerFunc)
+		}
+
+		if err != nil {
+			s.log.Fatal(ctx, fmt.Sprintf(schedulerDoneError, conf.Name, err))
+		}
+	}
+}
+
+func (s *scheduler) taskWrapper(conf SchedulerTaskConf, task handlerFunc) func() {
+	return func() {
+		ctx := s.createContext(conf)
+		s.log.Info(ctx, fmt.Sprintf(schedulerRunning, conf.Name))
+
+		if err := task(ctx); err != nil {
+			s.log.Error(ctx, fmt.Sprintf(schedulerDoneError, conf.Name, err))
+		} else {
+			s.log.Info(ctx, fmt.Sprintf(schedulerDoneSuccess, conf.Name))
+		}
+
+		startTime := appcontext.GetRequestStartTime(ctx)
+		s.log.Info(ctx, fmt.Sprintf(scheduleTimeExecution, conf.Name, startTime))
+	}
+}
---
+++ src/handler/scheduler/scheduler.go
@@ -0,0 +1,67 @@
+package scheduler
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/NupalHariz/DD/src/business/usecase"
+	"github.com/NupalHariz/DD/src/utils/config"
+	"github.com/go-co-op/gocron"
+	"github.com/reyhanmichiels/go-pkg/v2/log"
+)
+
+var (
+	once = &sync.Once{}
+)
+
+type Interface interface {
+	Run()
+}
+
+type SchedulerTaskConf struct {
+	Name             string
+	Enabled          bool
+	TimeType         string
+	Interval         time.Duration
+	Weekday          time.Weekday
+	ScheduledTime    string
+	MultipleSchedule []string
+}
+
+type scheduler struct {
+	cron     *gocron.Scheduler
+	metaConf config.ApplicationMeta
+	log      log.Interface
+	uc       *usecase.Usecases
+}
+
+type InitParam struct {
+	MetaConf config.ApplicationMeta
+	Log      log.Interface
+	Uc       *usecase.Usecases
+}
+
+func Init(param InitParam) Interface {
+	s := &scheduler{}
+
+	once.Do(func() {
+		cron := gocron.NewScheduler(time.Local)
+		cron.TagsUnique()
+
+		s = &scheduler{
+			metaConf: param.MetaConf,
+			log:      param.Log,
+			uc:       param.Uc,
+		}
+	})
+
+	return s
+}
+
+func (s *scheduler) assignScheduledTask() {}
+
+func (s *scheduler) Run() {
+	s.cron.StartAsync()
+	s.log.Info(context.Background(), "scheduler is running on background")
+}
