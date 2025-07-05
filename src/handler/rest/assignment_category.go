5ad167e1904e83803b57207e53006c9d21460770
Naufal Haris <naufal.michiels@gmail.com>
2025-07-05 23:39:05 +0700

    docs: add documentation for assignment category

--- src/handler/rest/assignment_category.go
+++ src/handler/rest/assignment_category.go
@@ -6,6 +6,17 @@ import (
 	"github.com/reyhanmichiels/go-pkg/v2/codes"
 )
 
+// @Summary Create Assignment Category
+// @Description Create a new assignment category
+// @Tags Assignment Category
+// @Security BearerAuth
+// @Param data body dto.CreateAssignmentCategory true "New Assignment Category"
+// @Produce json
+// @Success 200 {object} entity.HTTPResp{}
+// @Failure 400 {object} entity.HTTPResp{}
+// @Failure 404 {object} entity.HTTPResp{}
+// @Failure 500 {object} entity.HTTPResp{}
+// @Router /v1/assignment-categories [POST]
 func (r *rest) CreateAssignmentCategory(ctx *gin.Context) {
 	var param dto.CreateAssignmentCategory
 
@@ -23,6 +34,16 @@ func (r *rest) CreateAssignmentCategory(ctx *gin.Context) {
 	r.httpRespSuccess(ctx, codes.CodeCreated, nil, nil)
 }
 
+// @Summary Get All Assignment Category
+// @Description Get all a new assignment category
+// @Tags Assignment Category
+// @Security BearerAuth
+// @Produce json
+// @Success 200 {object} entity.HTTPResp{data=[]dto.GetAllAssignmentCategoryResponse}
+// @Failure 400 {object} entity.HTTPResp{}
+// @Failure 404 {object} entity.HTTPResp{}
+// @Failure 500 {object} entity.HTTPResp{}
+// @Router /v1/assignment-categories [GET]
 func (r *rest) GetAllAssignmentCategory(ctx *gin.Context) {
 	res, err := r.uc.AssignmentCategory.GetAll(ctx.Request.Context())
 	if err != nil {
